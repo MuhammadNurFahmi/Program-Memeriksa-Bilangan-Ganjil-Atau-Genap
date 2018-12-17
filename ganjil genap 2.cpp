4d79916236d84f7d260440d2ee3078a32165e2f8
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 11:32:56 +0700

    Add files via upload

---
+++ ganjil genap 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+main()
+{
+    int nilai,hasil;
+    cout<<"menentukan ganjil dan genap"<<endl;
+    cout<<"masukkan nilainya = ";
+    cin>>nilai;
+    hasil=nilai%2;
+    cout<<"hasilnya adalah = ";
+    if (hasil==0)
+        cout<<"genap";
+    else
+        cout<<"ganjil";
+    
+	return 0;
+}
+ 
+ 
+
